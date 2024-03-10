691d8414f8cdd96213b552ed4a6a250ce4f19a37
Sean Pitchford <sean.c.pitchford@gmail.com>
2024-03-10 17:14:16 -0400

    Rating view has all current data displayed.

--- src/components/pages/destination/DestinationRating.vue
+++ src/components/pages/destination/DestinationRating.vue
@@ -6,16 +6,163 @@
             <v-container v-if="isLoading">
                 <h3>Is Loading...</h3>
             </v-container>
-            <v-container v-else>
-                <h3>Destination Rating for {{ responseData.city }}, {{ responseData.state }}</h3>
-                <br />
-                <v-row>
-                    <v-rating v-model="rating" half-increments hover active-color="blue"
-                        color="orange-lighten-1"></v-rating>
-                    &nbsp;&nbsp;
-                    <v-btn color="primary" type="submit">Update Rating</v-btn>
-                </v-row>
-            </v-container>
+            <div v-else>
+                <v-container>
+                    <h3>Destination Rating for {{ responseData.city }}, {{ responseData.state }}</h3>
+                    <br />
+                    <v-row>
+                        <v-rating v-model="rating" half-increments hover active-color="blue"
+                            color="orange-lighten-1"></v-rating>
+                        &nbsp;&nbsp;
+                        <v-btn color="primary" type="submit">Update Rating</v-btn>
+                    </v-row>
+                    <v-row>
+
+                        <v-checkbox cols="3" :disabled="true" label="Amusment Park"
+                            v-model="responseData.hasAmusementPark"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Aquarium"
+                            v-model="responseData.hasAquarium"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Artistic Plays"
+                            v-model="responseData.hasArtisticsPlays"></v-checkbox>
+                    </v-row>
+                    <v-row>
+                        <v-checkbox cols="3" :disabled="true" label="Beach" v-model="responseData.hasBeach"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Casino" v-model="responseData.hasCasino"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Mountains"
+                            v-model="responseData.hasMountains"></v-checkbox>
+                    </v-row>
+                    <v-row>
+                        <v-checkbox cols="3" :disabled="true" label="Musuems" v-model="responseData.hasMuseum"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Night Life"
+                            v-model="responseData.hasNightLife"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Outdoor Activities"
+                            v-model="responseData.hasOutdoorActivities"></v-checkbox>
+                    </v-row>
+                    <v-row>
+                        <v-checkbox cols="3" :disabled="true" label="Skiing" v-model="responseData.hasSkiing"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Sports Stadiums"
+                            v-model="responseData.hasSportStadium"></v-checkbox>
+                        <v-checkbox cols="3" :disabled="true" label="Zoos" v-model="responseData.hasZoo"></v-checkbox>
+                    </v-row>
+                    <v-row v-if="responseData?.aquariumResults?.length > 0">
+                        <h4>Aquarium Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.aquariumResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.gamblingResults?.length > 0">
+                        <h4>Gambling Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.gamblingResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.landMarkResults?.length > 0">
+                        <h4>Landmark Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.landMarkResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.museumResults?.length > 0">
+                        <h4>Museum Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.museumResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.nightLifeResults?.length > 0">
+                        <h4>Night Life Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.nightLifeResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.restaurantResults?.length > 0">
+                        <h4>Restaurant Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.restaurantResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.skiingResults?.length > 0">
+                        <h4>Skiing Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.skiingResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.theaterResults?.length > 0">
+                        <h4>Theater Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.theaterResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                    <v-row v-if="responseData?.zooResults?.length > 0">
+                        <h4>Zoo Results</h4>
+                        <v-data-table :headers="headers" :items="responseData.zooResults" item-key="name"
+                            class="elevation-1">
+                            <template v-slot:item="{ item }">
+                                <tr>
+                                    <td>{{ item.name }}</td>
+                                    <td>{{ item.type }}</td>
+                                    <td>{{ item.address }}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-row>
+                </v-container>
+            </div>
         </v-main>
     </v-layout>
 </template>
@@ -23,7 +170,7 @@
 <script>
 import axios from '../../../services/axios'
 import { useToast } from "vue-toastification"
-import { VContainer, VLayout, VMain, VRating, VRow, VBtn } from 'vuetify/lib/components'
+import { VContainer, VLayout, VMain, VRating, VRow, VBtn, VCheckbox, VDataTable } from 'vuetify/lib/components'
 import Navbar from '../../partial/Navbar.vue'
 import SideNavDrawer from '../../partial/SideNavDrawer.vue'
 const toast = useToast()
@@ -34,7 +181,12 @@ export default {
         return {
             responseData: {},
             isLoading: false,
-            rating: 0
+            rating: 0,
+            headers: [
+                { title: 'Name', value: 'name' },
+                { title: 'Type', value: 'type' },
+                { title: 'Address', value: 'address' }
+            ],
         }
     },
     created() {
@@ -51,6 +203,8 @@ export default {
         VRating,
         VRow,
         VBtn,
+        VCheckbox,
+        VDataTable,
         SideNavDrawer
     }, methods: {
         async fetchData(id) {
